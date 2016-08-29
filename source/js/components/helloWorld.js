21af34697238a605a49c0205e5c3e9efbed771c5
Wilbert Schepenaar <wilbertschepenaar@hotmail.com>
2016-08-29 13:57:26 +0200

    moved globals to entry js file

--- source/js/components/helloWorld.js
+++ source/js/components/helloWorld.js
@@ -1,13 +1,3 @@
-// enable jQuery:
-// $ npm install jquery --save
-// uncomment following line:
-window.$ = window.jQuery = require("jquery");
-
-// enable underscore
-// $ npm install underscore --save
-// uncomment following line:
-// window._ = require("underscore");
-
 const helloWorld = "Hello World!";
 $('main').css('background', 'red');
 
--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,12 @@
+// enable jQuery:
+// $ npm install jquery --save
+// uncomment following line:
+window.$ = window.jQuery = require("jquery");
+
+// enable underscore
+// $ npm install underscore --save
+// uncomment following line:
+// window._ = require("underscore");
+
 var helloWorld = require('./components/helloWorld');
 console.log(helloWorld);
